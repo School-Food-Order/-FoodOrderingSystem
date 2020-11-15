5c503354e235b9e69cddf2c0596e7579cdf31d8b
William Castleman <wc104@canterbury.ac.uk>
2020-11-15 19:33:01 +0000

    Checkout button on CustomerOrderScreen hides the form while the CheckoutScreen is open Checkout button on CheckoutScreen gives user prompt that order has been sent to kitchen then closes and reopens an empty CustomerOrderScreen

--- test/CheckoutScreen.cs
+++ test/CheckoutScreen.cs
@@ -12,8 +12,10 @@ namespace test
 {
     public partial class CheckoutScreen : Form
     {
+
+        OrderScreen oS;
         
-        public CheckoutScreen(float total, ListBox list)
+        public CheckoutScreen(float total, ListBox list, OrderScreen oScreen)
         {
             InitializeComponent();
             totalTextBox.Text = total.ToString();
@@ -23,13 +25,16 @@ namespace test
                 text = item.ToString(); // /n to print each item on new line or you omit /n to print text on same line
                 checkoutListBox.Items.Add(text);
             }
-            
+            oS = oScreen;
         }
 
 
         private void checkoutButton_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Thank-you for placing your order, it has been sent to the kitchen to be prepared!", "Order Confirmation", MessageBoxButtons.OK);
+            oS.clearList();
+            oS.Show();
+            this.Dispose();
         }
 
     }
