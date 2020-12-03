7a3256418afc0aad14bb062120aa1ed972a48c25
mm1089 <72493335+mm1089@users.noreply.github.com>
2020-12-03 21:04:29 +0000

    Created CustomerLiveOrderScreen with listboxes for live orders. No functionality added yet, added sprite for fun.

--- test/Properties/Resources.Designer.cs
+++ test/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace test.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap iconHappyGuy {
+            get {
+                object obj = ResourceManager.GetObject("iconHappyGuy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap iconSubroll {
             get {
                 object obj = ResourceManager.GetObject("iconSubroll", resourceCulture);
